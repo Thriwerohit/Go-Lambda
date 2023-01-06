454ed03759cac561785fa1a091c9099024491ba0
Rohit Kumar <rohit.kumar@thriwe.com>
2023-01-06 17:20:50 +0530

    code structure changed

---
+++ goLambda/goLambda.go
@@ -0,0 +1,296 @@
+package golambda
+
+import (
+	"fmt"
+	"ruleEngine/httpClient"
+	"strings"
+	"time"
+)
+
+type Event struct {
+	ObjectID           string `json:"objectId"`
+	ProjectID          string `json:"projectId"`
+	GlobalEventDetails struct {
+		ID       int    `json:"id"`
+		RuleName string `json:"ruleName"`
+	} `json:"globalEventDetails"`
+	Name         string `json:"name"`
+	IsRepeat     bool   `json:"isRepeat"`
+	RepeatDetail struct {
+		Value int    `json:"value"`
+		Unit  string `json:"unit"`
+	} `json:"repeatDetail"`
+	CoinAmount     int       `json:"coinAmount"`
+	CoinExpiry     time.Time `json:"coinExpiry"`
+	IsActive       bool      `json:"isActive"`
+	RuleExpiryDate time.Time `json:"ruleExpiryDate"`
+	EventDate      time.Time `json:"eventDate"`
+	CreatedAt      time.Time `json:"createdAt"`
+	UpdatedAt      time.Time `json:"updatedAt"`
+	UserDetails    []struct {
+		UserID      string    `json:"userId"`
+		DateOfBirth time.Time `json:"dateOfBirth"`
+	} `json:"userDetails"`
+}
+
+type ParseEventResponse struct {
+	Results []struct {
+		Event
+	} `json:"results"`
+}
+type Users struct {
+	ObjectID     string `json:"objectId"`
+	ProjectId    string `json:"projectId"`
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+	LastName     string `json:"lastName"`
+	EmailID      string `json:"emailId"`
+	MobileNumber string `json:"mobileNumber"`
+	DateOfBirth  string `json:"dateOfBirth"`
+	Expendature  int    `json:"expendature"`
+}
+
+type ParseUsers struct {
+	Results []struct {
+		Users
+	} `json:"results"`
+}
+type AddCoinRequest struct {
+	ProjectId  string    `json:"projectId"`
+	UserId     string    `json:"userId"`
+	Coin       float64   `json:"coin"`
+	ExpiryDate time.Time `json:"expiryDate"`
+}
+type Coin struct {
+	ObjectID  string    `json:"objectId"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	ProjectID string    `json:"projectId"`
+	UserID    string    `json:"userId"`
+	Coins     []struct {
+		Amount     float64   `json:"amount"`
+		ExpiryDate time.Time `json:"expiryDate"`
+	} `json:"coins"`
+	TotalCoins int `json:"totalCoins"`
+}
+
+type ParseCoins struct {
+	Results []struct {
+		Coin
+	} `json:"results"`
+}
+type UpdateResponse []struct {
+	Success struct {
+		UpdatedAt time.Time `json:"updatedAt"`
+	} `json:"success"`
+}
+
+type SubtractCoinRequest struct {
+	ProjectId           string  `json:"projectId"`
+	UserId              string  `json:"userId"`
+	IsCoinsExpireReason bool    `json:"isCoinsExpireReason"`
+	Amount              float64 `json:"amount"`
+	Reason              string  `json:"reason"`
+}
+
+type ExpendatureRule struct {
+	ObjectID          string `json:"objectId"`
+	ProjectID         string `json:"projectId"`
+	GlobalRuleDetails struct {
+		ID       int    `json:"id"`
+		RuleName string `json:"ruleName"`
+	} `json:"globalRuleDetails"`
+	IsRepeat     bool   `json:"isRepeat"`
+	Name         string `json:"name"`
+	RepeatDetail struct {
+		Value int    `json:"value"`
+		Unit  string `json:"unit"`
+	} `json:"repeatDetail"`
+	CoinAmount        int       `json:"coinAmount"`
+	CoinExpiry        time.Time `json:"coinExpiry"`
+	IsActive          bool      `json:"isActive"`
+	RuleExpiryDate    time.Time `json:"ruleExpiryDate"`
+	MaxCoin           int       `json:"MaxCoin"`
+	ExpendatureAmount int       `json:"expendatureAmount"`
+	ConversionRate    float64   `json:"conversionRate"`
+}
+type ParseRules struct {
+	Results []struct {
+		ExpendatureRule
+	} `json:"results"`
+}
+
+func Handler() error {
+	// get all events across projectId
+	var eventResponse ParseEventResponse
+	var userResponse ParseUsers
+	var resp UpdateResponse
+	var coin ParseCoins
+
+	// check coin expiry date
+	body := strings.NewReader(`{}`)
+	_, errCoins := httpClient.PostParseClient("GET", "https://dev.ext-api.thriwe.com/parse/classes/coins", body, &coin)
+	if errCoins != nil {
+		return errCoins
+	}
+	for i := 0; i < len(coin.Results); i++ {
+		itr := coin.Results[i]
+		for j := 0; j < len(itr.Coins); j++ {
+			// check for current date and coin exp date
+			if itr.Coins[j].ExpiryDate.Day() == time.Now().Day() && itr.Coins[j].ExpiryDate.Month() == time.Now().Month() && itr.Coins[j].ExpiryDate.Year() == time.Now().Year() {
+				coinSubtractBody := strings.NewReader(`{
+					"projectId":"` + itr.ProjectID + `",
+					"userId":"` + itr.UserID + `",
+					"isCoinsExpireReason":` + fmt.Sprint(true) + `,
+					"amount":` + fmt.Sprint(itr.Coins[j].Amount) + `,
+					"reason": ""
+				 }`)
+				_, errSubtractCoins := httpClient.ParseClient("PUT", "http://localhost:8083/subtractCoinsLambda", coinSubtractBody, &resp)
+				if errSubtractCoins != nil {
+					return errSubtractCoins
+				}
+			}
+		}
+	}
+
+	_, errEvent := httpClient.ParseClient("GET", "https://dev-fab-api-gateway.thriwe.com/parse/classes/events", body, &eventResponse)
+	if errEvent != nil {
+		return errEvent
+	}
+
+	for j := 0; j < len(eventResponse.Results); j++ {
+
+		if eventResponse.Results[j].RuleExpiryDate.Before(time.Now()) || !eventResponse.Results[j].IsActive {
+			continue
+		}
+
+		if eventResponse.Results[j].GlobalEventDetails.ID == 1 {
+			if eventResponse.Results[j].EventDate.Day() == time.Now().Day() && eventResponse.Results[j].EventDate.Month() == time.Now().Month() {
+
+				body := strings.NewReader(`{
+					"where":{
+						"projectId":"` + eventResponse.Results[j].ProjectID + `"
+					}
+				}`)
+				_, errUser := httpClient.ParseClient("GET", "https://dev-fab-api-gateway.thriwe.com/parse/users", body, &userResponse)
+				if errUser != nil {
+					return errUser
+				}
+				for i := 0; i < len(userResponse.Results); i++ {
+					userId := userResponse.Results[i].ObjectID
+
+					date := eventResponse.Results[j].CoinExpiry
+					body := strings.NewReader(`{
+						"projectId":"` + eventResponse.Results[j].ProjectID + `",
+                        "userId":"` + userId + `",
+                        "coin":` + fmt.Sprint(eventResponse.Results[j].CoinAmount) + `,
+						"expiryDate":"` + date.Format("2006-01-02T15:04:05Z") + `",
+						"reason":"` + eventResponse.Results[j].GlobalEventDetails.RuleName + `",
+						"ruleId":"` + eventResponse.Results[j].ObjectID + `"
+					}`)
+					_, errCoins := httpClient.ParseClient("PUT", "http://localhost:8083/expendatureAddCoin", body, &resp)
+					if errCoins != nil {
+						return errCoins
+					}
+				}
+			}
+		} else if eventResponse.Results[j].GlobalEventDetails.ID == 2 {
+			body := strings.NewReader(`{
+				"where":{
+					"projectId":"` + eventResponse.Results[j].ProjectID + `"
+				}
+			}`)
+			_, errUser := httpClient.ParseClient("GET", "https://dev-fab-api-gateway.thriwe.com/parse/users", body, &userResponse)
+			if errUser != nil {
+				return errUser
+			}
+
+			for i := 0; i < len(userResponse.Results); i++ {
+
+				dateOfBirth, errTime := time.Parse("2006-01-02", userResponse.Results[i].DateOfBirth)
+
+				if errTime != nil {
+					return errTime
+				}
+
+				if dateOfBirth.Day() == time.Now().Day() && dateOfBirth.Month() == time.Now().Month() {
+					date := eventResponse.Results[j].CoinExpiry
+					userId := userResponse.Results[i].ObjectID
+					body := strings.NewReader(`{
+						"projectId":"` + eventResponse.Results[j].ProjectID + `",
+                        "userId":"` + userId + `",
+                        "coin":` + fmt.Sprint(eventResponse.Results[j].CoinAmount) + `,
+						"expiryDate":"` + date.Format("2006-01-02T15:04:05Z") + `",
+						"reason":"` + eventResponse.Results[j].GlobalEventDetails.RuleName + `",
+						"ruleId":"` + eventResponse.Results[j].ObjectID + `"
+					}`)
+					_, errCoins := httpClient.ParseClient("PUT", "http://localhost:8083/expendatureAddCoin", body, &resp)
+					if errCoins != nil {
+						return errCoins
+					}
+				}
+			}
+		}
+	}
+
+	if int(time.Now().Local().Weekday()) == 6 {
+		projects, err := Projects("Weekly")
+		if err != nil {
+			return err
+		}
+		if len(projects.Results) == 0 {
+			fmt.Println("No weekly projects found")
+		}
+		prevProjectId := ""
+		for i := 0; i < len(projects.Results); i++ {
+			if projects.Results[i].ProjectID != prevProjectId {
+				body := strings.NewReader(`{
+					"projectId":"` + projects.Results[i].ProjectID + `"
+				}`)
+				_, err := httpClient.ParseClient("PUT", "http://localhost:8083/expendatureAddCoin", body, &resp)
+				if err != nil {
+					return err
+				}
+				prevProjectId = projects.Results[i].ProjectID
+			}
+		}
+	}
+	if int(time.Now().Local().Day()) == 1 {
+		projects, err := Projects("Monthly")
+		if err != nil {
+			return err
+		}
+		if len(projects.Results) == 0 {
+			fmt.Println("No weekly projects found")
+		}
+		prevProjectId := ""
+		for i := 0; i < len(projects.Results); i++ {
+			if projects.Results[i].ProjectID != prevProjectId {
+				body := strings.NewReader(`{
+					"projectId":"` + projects.Results[i].ProjectID + `"
+				}`)
+				_, err := httpClient.ParseClient("PUT", "http://localhost:8083/expendatureAddCoin", body, &resp)
+				if err != nil {
+					return err
+				}
+				prevProjectId = projects.Results[i].ProjectID
+			}
+		}
+	}
+
+	return nil
+}
+
+func Projects(repeat string) (*ParseRules, error) {
+	var rules ParseRules
+	body := strings.NewReader(`{
+		"where":{
+			"repeatDetail.unit":"` + repeat + `"
+		}
+	}`)
+	_, errProjects := httpClient.ParseClient("GET", "https://dev-fab-api-gateway.thriwe.com/parse/classes/rules", body, &rules)
+	if errProjects != nil {
+		return nil, errProjects
+	}
+	return &rules, nil
+}
